ad6ba34920336d39fbf2b142cc9c3d12ecd44300
bcckaushalya <bcckaushalya@gmail.com>
2024-09-06 17:43:36 +0300

    Q3 Activated
    Q3 - toggle line comments.

--- src/App.java
+++ src/App.java
@@ -1,19 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int age = 100;
+        int age = 10;
         int n1 = 1;
         
         //Q3
 
-        // if (age > 0 && age < 18) {
-        //     System.out.println("You are underage");
-        // }
-        // else if (age >= 65) {
-        //     System.out.println("You are retired");
-        // }
-        // else 
-        // System.out.println("You are an adult");
+        if (age > 0 && age < 18) {
+            System.out.println("You are underage");
+        }
+        else if (age >= 65) {
+            System.out.println("You are retired");
+        }
+        else 
+        System.out.println("You are an adult");
         
 
         //Q5
